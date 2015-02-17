d6aebd9474cff7bf13de7d755abb3fb0b2e5a965
Joshua LeBlanc <jleblan8@unb.ca>
2015-02-17 18:52:08 -0400

    submit and game screen

--- main.rb
+++ main.rb
@@ -2,24 +2,25 @@ require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/static_assets'
 require 'sinatra/cookies'
+require 'data_mapper'
 require 'haml'
 require 'savon'
 require 'encryptor'
 require 'pony'
+require 'tassadar'
 
 module Site
 	class App < Sinatra::Application
+		DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/data.db")
+
 		Dir["./app/models/*.rb"].each { |file| require file }
 		Dir["./app/helpers/*.rb"].each { |file| require file }
 		Dir["./app/controllers/*.rb"].each { |file| require file }
 
-		use Routes::Account
-		use Routes::Error
 		use Routes::Index
-		use Routes::Login
-		use Routes::Recovery
-		use Routes::Wants
-		use Routes::PurchaseHistory
-		use Routes::Logout
+		use Routes::Submit
+		use Routes::Games
+		DataMapper.auto_upgrade!
+		DataMapper.finalize
 	end
 end
