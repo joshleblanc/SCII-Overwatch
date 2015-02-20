059a7526796adcb05e75be8f0fe0c4e517a3c8e1
Joshua LeBlanc <jleblan8@unb.ca>
2015-02-19 20:47:22 -0400

    oh god who lets me program

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -8,36 +8,30 @@ module Site
 				p Game.all
 				@game = Game.first(id: params[:id])
 				@player = @game.player
-				@can_vote = !@game.voters.get(request.ip).nil?
+				@can_vote = @game.voters.get(request.ip).nil?
 				p @game.voters
 				p request.ip
 				render_page :game
 			end
 
-			get '/game/:id/guilty' do
+			post '/game/:id' do
 				game = Game.first(id: params[:id])
 				if params[:winner_confirm] != game.winner
 					Voter.create(ip: request.ip)
 					redirect to "#{request.referrer}?error=incorrect_winner"
 				end
-				if game.voters.get(request.ip).nil?
-					Voter.create(ip: request.ip, game: game)
-					game.player.update(guilty_count: game.player.guilty_count + 1)
-				end
-				redirect to '/list/recent'
-			end
 
-			get '/game/:id/innocent' do
-				game = Game.first(id: params[:id])
-				if params[:winner_confirm] != game.winner
-					Voter.create(ip: request.ip)
-					redirect to "#{request.referrer}?error=incorrect_winner"
-				end
 				if game.voters.get(request.ip).nil?
 					Voter.create(ip: request.ip, game: game)
-					game.player.update(innocent_count: game.player.innocent_count + 1)
+					if params["verdict"] == "Guilty"
+						game.player.update(guilty_count: game.player.guilty_count + 1)
+					end
+					if params["verdict"] == "Innocent"
+						game.player.update(innocent_count: game.player.innocent_count + 1)
+					end
 				end
 				redirect to '/list/recent'
+
 			end
 
 			get '/game/download/:id' do
